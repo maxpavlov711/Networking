71988a3d93b290f714f1496d936fe3087d51c13a
Max Pavlov <max.pavlov711@gmail.com>
2022-02-10 13:04:41 +0300

    POST Request

--- Networking/ViewController.swift
+++ Networking/ViewController.swift
@@ -34,7 +34,30 @@ class ViewController: UIViewController {
     }
     
     @IBAction func postRequest(_ sender: Any) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
         
+        let userData = ["Course": "Networking", "Lesson": "GET and PAST Request"]
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: userData, options: []) else { return }
+        request.httpBody = httpBody
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { data, resposne, error in
+            guard let resposne = resposne, let data = data else { return }
+            
+            print(resposne)
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }.resume()
     }
     
 }
